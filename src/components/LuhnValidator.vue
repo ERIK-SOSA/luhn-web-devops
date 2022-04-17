8f34d6d0995224893ab8232f30c89226e45c11d7
ameridass <ameridas1998@gmail.com>
2022-04-17 17:22:40 -0600

    Componente agreagado para el servicio de Luhn

---
+++ src/components/LuhnValidator.vue
@@ -0,0 +1,15 @@
+<template>
+<v-container>
+  <p>El servicio de  Luhn </p>
+</v-container>
+</template>
+
+<script>
+export default {
+  name: "LuhnValidator"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/InicioView.vue
@@ -0,0 +1,15 @@
+<template>
+ <LuhnValidator />
+</template>
+
+<script>
+import LuhnValidator from "@/components/LuhnValidator";
+export default {
+  name: "InicioView",
+  components: {LuhnValidator}
+}
+</script>
+
+<style scoped>
+
+</style>
