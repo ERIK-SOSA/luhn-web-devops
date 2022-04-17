0582cf1ae1e21660a03944128582748bf2ff7ad5
ameridass <ameridas1998@gmail.com>
2022-04-17 16:40:13 -0600

    Login validado con post request para igreso a home.

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <div class="background"></div>
     <v-main>
       <router-view/>
     </v-main>
@@ -53,3 +17,14 @@ export default {
   }),
 };
 </script>
+<style>
+  .background {
+    background-image: url(https://cdn.wallpapersafari.com/2/99/h3kqmP.jpg) !important;
+    height: 300px;
+    width: 100%;
+    display: block;
+    top: 0;
+    position: absolute;
+    background-size: cover;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,151 +1,87 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-row>
+    <v-col cols="10" lg="4" class="mx-auto">
+      <v-card class="form-center pa-4">
+        <div class="text-center">
+          <v-avatar size="100" color="indigo lighten-4">
+            <v-icon size="40" color="indigo">mdi-account</v-icon>
+          </v-avatar>
+          <h2 class="indigo--text">Login Page</h2>
+        </div>
+        <v-form @submit.prevent="submitHandler" ref="form">
+          <v-card-text>
+            <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                type="email"
+                label="Email"
+                placeholder="Email"
+                prepend-inner-icon="mdi-account">
+            </v-text-field>
+            <v-text-field
+                v-model="password"
+                :rules="passwordRules"
+                :type="passwordShow ? 'text': 'password'"
+                label="Password"
+                placeholder="Password"
+                prepend-inner-icon="mdi-key"
+                :append-icon="passwordShow ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="passwordShow = !passwordShow"
+            >
+            </v-text-field>
+          </v-card-text>
+          <v-card-actions class="justify-center">
+            <v-btn :loading="loading" type="submit" color="indigo">
+              <span class="white--text px-8">LOGIN</span>
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+import axios from "axios";
 
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    passwordShow: false,
+    loading: false,
+    password: '',
+    passwordRules: [
+      v => !!v || 'La contraseña es requerida',
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'El Email es requerido',
+      v => /.+@.+\..+/.test(v) || 'El Email debe de ser valido'
+    ]
+  }),
+  methods: {
+    submitHandler() {
+      this.loading = true;
+      if(this.$refs.form.validate()){
+        this.setLogin();
+      }
+      this.loading = false;
+    },
+    setLogin(){
+      let data = {
+        email: this.email,
+        password: this.password
+      }
+      axios.post("http://localhost:3000/login", data).then((response) => {
+        console.log(response.data);
+      })
+    }
   }
+}
 </script>
+<style>
+.form-center {
+  margin-top: 40%;
+}
+</style>
